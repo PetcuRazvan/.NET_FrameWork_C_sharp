69d17637b50dad96e405b2386f53766d8127ad80
unknown <petcurazvam012@gmail.com>
2026-03-12 15:42:05 +0200

    am modificat proiectul Curs3 WinForms

--- Curs3 Windows Forms/Curs3 Windows Forms/Form1.Designer.cs
+++ Curs3 Windows Forms/Curs3 Windows Forms/Form1.Designer.cs
@@ -28,52 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.e1 = new System.Windows.Forms.TextBox();
+            this.pe1 = new System.Windows.Forms.TextBox();
+            this.lb = new System.Windows.Forms.ListBox();
+            this.err = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.err)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(212, 215);
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(28, 115);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(163, 99);
+            this.button1.Size = new System.Drawing.Size(285, 35);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Apasa-ma";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // checkBox1
+            // e1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(70, 156);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(113, 24);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.e1.Location = new System.Drawing.Point(28, 62);
+            this.e1.Name = "e1";
+            this.e1.Size = new System.Drawing.Size(285, 26);
+            this.e1.TabIndex = 1;
+            this.e1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.e1_KeyPress);
             // 
-            // button2
+            // pe1
             // 
-            this.button2.Location = new System.Drawing.Point(70, 215);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(107, 99);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.pe1.BackColor = System.Drawing.SystemColors.Window;
+            this.pe1.Location = new System.Drawing.Point(28, 183);
+            this.pe1.Name = "pe1";
+            this.pe1.ReadOnly = true;
+            this.pe1.Size = new System.Drawing.Size(285, 26);
+            this.pe1.TabIndex = 2;
+            // 
+            // lb
+            // 
+            this.lb.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb.FormattingEnabled = true;
+            this.lb.ItemHeight = 23;
+            this.lb.Location = new System.Drawing.Point(28, 245);
+            this.lb.Name = "lb";
+            this.lb.Size = new System.Drawing.Size(285, 188);
+            this.lb.TabIndex = 3;
+            this.lb.SelectedIndexChanged += new System.EventHandler(this.lb_SelectedIndexChanged);
+            // 
+            // err
+            // 
+            this.err.ContainerControl = this;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(612, 496);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.checkBox1);
+            this.ClientSize = new System.Drawing.Size(345, 496);
+            this.Controls.Add(this.lb);
+            this.Controls.Add(this.pe1);
+            this.Controls.Add(this.e1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_load);
+            ((System.ComponentModel.ISupportInitialize)(this.err)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,8 +101,10 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox e1;
+        private System.Windows.Forms.TextBox pe1;
+        private System.Windows.Forms.ListBox lb;
+        private System.Windows.Forms.ErrorProvider err;
     }
 }
 
--- Curs3 Windows Forms/Curs3 Windows Forms/Form1.cs
+++ Curs3 Windows Forms/Curs3 Windows Forms/Form1.cs
@@ -16,17 +16,60 @@ namespace Curs3_Windows_Forms
         public Form1()
         {
             InitializeComponent();
+            obe1 = new Element();
             obe1.Ev_Mod_Valoare += Obe1_Ev_Mod_Valoare;
         }
 
         private void Obe1_Ev_Mod_Valoare(Element obj)
         {
-            throw new NotImplementedException();
+            pe1.Text = obj.Patrat.ToString();
+            string s = $"{obj.Valoare,6}|{obj.Patrat,6}|"; //6 ne pastreaza valoarea incadrata intr-un segment de lungime 6
+            lb.Items.Add(s); //adaugam calculul in list box
+            //cel mai top font este Consolas, spatiile au aceeasi marime si de astea, e aliniat totul
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+                obe1.Valoare = int.Parse(e1.Text);
+                lb.SelectedIndex = -1;
+                err.SetError(e1, null); //face ca eroarea sa dispara
+            } 
+            catch (FormatException ex)
+            {
+                //MessageBox.Show(ex.Message, "ERR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //putem face asa, sau
+                err.SetError(e1, "invalid");
+            }
+            
+        }
 
+        public void Form1_load(object sender, EventArgs e)
+        { 
+            e1.Text = obe1.Valoare.ToString();
+            pe1.Text = obe1.Patrat.ToString();
+        }
+
+        private void e1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return) //return = enter, este si enter dar unele tastaturi au return inloc de enter
+            {
+                button1_Click(null, null); //nu ne intereseaza nici senderul, nici event args
+                e1.SelectAll(); //ca de fiecare data cand dau enter, sa mi selecteze tot textul si sa putem scrie automat urmatoruk nr in textbox
+            }
+        }
+
+        private void lb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lb.SelectedIndex == -1) //pt ca daca apasam pe buton si ne dispare selectia pe lista(ca asa e mai nice), ne-ar da eroare codul de mai jos pt ca nu e selectat nmc
+            {
+                return;
+            }
+
+            string sel = lb.SelectedItem as string;
+            e1.Text = (sel.Split('|')[0]).Trim();
+            pe1.Text = (sel.Split('|')[1]).Trim();
         }
     }
 }
